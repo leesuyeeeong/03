5ba0f7c55b36a792d05c5725f2ad7dd6a87865ec
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-09-21 22:53:26 +0900

    exercise 3(last)

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-
 int main(int argc, char *argv[]){
 	char text;
 	printf("enter a character : ");
 	scanf("%c", &text);
 
 	printf("next character of %c (%d) is %c(%d)\n", text, text, text+1, text+1);
+
+	return 0;
 }
