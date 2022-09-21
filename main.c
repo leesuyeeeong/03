a37d1820d328cc27f3df11c4fde25a42b0de161f
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-09-21 20:11:40 +0900

    exercise 4

--- main.c
+++ main.c
@@ -3,12 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]){
 	char text;
-	printf("input a number : ");
-	scanf("%c", &text);
+int main()
+{
+	int num1, num2;
+	printf("���ڸ� �Է��ϼ��� : ");
+	scanf_s("%d", &num1);
+	printf("�и� �Է��ϼ��� : ");
+	scanf_s("%d", &num2);
 	
-	printf("next charater of %c (%d) is %c(%d)\n", text, text, text+1, text+1);
+	if (num2 != 0){
+		if (num1 % num2){
+			printf("�������� ����� %d / %d = %f\n�Դϴ�.", num1, num2, num1 / (float)num2);
 	
-	return 0;
+		}else{
+			printf("�������� ����� %d / %d = %d\n�Դϴ�.", num1, num2, num1/num2);
+		}
+	} 
+	return 0;	
 }
