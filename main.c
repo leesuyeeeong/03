ac9c89154ae9e6ab8558d27ac04daf930e59844d
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-09-21 22:37:27 +0900

    exercise2

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	char c;
 	int i;
@@ -11,7 +11,7 @@ int main(void)
 	printf("input a number : ");
 	scanf("%c",&c);
 	
-	i = c-0;
+	i = (c-0);
 	//text-0
 	//"%c",&text
 	printf("The input number is %i\n",i);
