3a6a2c87ea402caac66d06269b16be13e6880f5b
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-09-21 22:34:11 +0900

    exercise2

--- main.c
+++ main.c
@@ -3,21 +3,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main()
+int main(void)
 {
-	int num1, num2;
-	printf("분자를 입력하세요 : ");
-	scanf_s("%d", &num1);
-	printf("분모를 입력하세요 : ");
-	scanf_s("%d", &num2);
+	char c;
+	int i;
 	
-	if (num2 != 0){
-		if (num1 % num2){
-			printf("나누기의 결과는 %d / %d = %f\n입니다.", num1, num2, num1 / (float)num2);
+	printf("input a number : ");
+	scanf("%c",&c);
 	
-		}else{
-			printf("나누기의 결과는 %d / %d = %d\n입니다.", num1, num2, num1/num2);
-		}
-	} 
-	return 0;	
+	i = c-0;
+	//text-0
+	//"%c",&text
+	printf("The input number is %i\n",i);
+	
+	return 0; 	
 }
